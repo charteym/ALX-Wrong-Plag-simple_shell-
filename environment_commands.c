555396ddd49053aa938e022ef3a3ae39b8d4a595
charteym <125804133+charteym@users.noreply.github.com>
2023-05-24 17:27:03 +0000

    Update environment_commands.c

--- environment_commands.c
+++ environment_commands.c
@@ -3,7 +3,7 @@
 /**
  * _myenv - Prints the current environment variables.
  * @info: The structure containing potential arguments.
- * 
+ *
  * Return: Always 0.
  */
 int _myenv(info_t *info)
@@ -16,7 +16,7 @@ int _myenv(info_t *info)
  * _getenv - Retrieves the value of an environment variable.
  * @info: The structure containing potential arguments.
  * @name: The name of the environment variable.
- * 
+ *
  * Return: The value of the environment variable.
  */
 char *_getenv(info_t *info, const char *name)
@@ -37,7 +37,7 @@ char *_getenv(info_t *info, const char *name)
 /**
  * _mysetenv - Initializes a new environment variable or modifies an existing one.
  * @info: The structure containing potential arguments.
- * 
+ *
  * Return: Always 0.
  */
 int _mysetenv(info_t *info)
@@ -55,35 +55,35 @@ int _mysetenv(info_t *info)
 /**
  * _myunsetenv - Removes an environment variable.
  * @info: The structure containing potential arguments.
- * 
+ *
  * Return: Always 0.
  */
 int _myunsetenv(info_t *info)
 {
     int i;
-
+    
     if (info->argc == 1)
     {
         _eputs("Too few arguments.\n");
         return (1);
     }
     for (i = 1; i <= info->argc; i++)
-        _unsetenv(info, info->argv[i]);
-
-    return (0);
+        _unsetenv(info, info->argv[i])
+        
+        return (0);
 }
 
 /**
  * populate_env_list - Populates the environment linked list.
  * @info: The structure containing potential arguments.
- * 
+ *
  * Return: Always 0.
  */
 int populate_env_list(info_t *info)
 {
     list_t *node = NULL;
     size_t i;
-
+    
     for (i = 0; environ[i]; i++)
         add_node_end(&node, environ[i], 0);
     info->env = node;
